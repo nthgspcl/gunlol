3dfbf30fdb0aac9623370019e6a13935888f9b02
nthgspcl <127815437+nthgspcl@users.noreply.github.com>
2026-06-12 16:17:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  const startMessage = "Click here to see the motion baby";
+  const startMessage = "CHAN BỐ MÀY ĐI";
   let startTextContent = '';
   let startIndex = 0;
   let startCursorVisible = true;
