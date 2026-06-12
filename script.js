a3f0e5dff3c4e9289e64b07afd005ada3f6d3853
nthgspcl <127815437+nthgspcl@users.noreply.github.com>
2026-06-12 16:17:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
-  const name = "JAQLIV";
+  const name = "SUNI IS ME";
   let nameText = '';
   let nameIndex = 0;
   let isNameDeleting = false;
